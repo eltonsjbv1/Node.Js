54a2926554eefd7dbd8acb8feb0df660d277974c
Elton Nunes <soh_cs@hotmail.com>
2020-04-25 11:24:27 -0300

    started project guiapressblog v1

---
+++ GuiaPressBlog/articles/Article.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+const Category = require("../categories/Category")
+
+const Article = connection.define('articles', {
+    title:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },slug: {
+
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    body: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+
+})
+
+Category.hasMany(Article); //Uma categoria pertence a muitas categorias
+Article.belongsTo(Category); //Um artigo pertence a uma categoria
+
+
+
+module.exports = Article;
---
+++ GuiaPressBlog/articles/ArticlesController.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const Category = require("../categories/Category");
+
+router.get("/articles", (req, res) => {
+    res.send("rota de artigos")
+
+
+});
+
+router.get("/admin/articles/new", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {categories: categories});
+    })
+    
+
+
+});
+
+
+module.exports = router;
